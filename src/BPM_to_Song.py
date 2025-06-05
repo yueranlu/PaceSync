86d6e3576ef6d48e51ccc02652d2011888bb8d8c
endemm <endemm09@gmail.com>
2025-06-05 16:06:13 -0400

    coded the recommendations from bpm and track history

---
+++ src/BPM_to_Song.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+class Song(BaseModel):
+    name: str
+    length: int
+    artist: str
+
+app = FastAPI()
+
+sp = spotipy.Spotify(auth_manager = SpotifyOAuth(client_id="b55d399d11934dfc9cc4941ef3d40e2b",
+                                                 client_secret="56cf7f92503942a8bcca70b53de6675b",
+                                                 redirect_uri="http://127.0.0.1:8000/auth_fail",
+                                                 scope="user-top-read")) #apparently this is needed for recommendations
+
+track_history = []
+
+#global function that finds a suitable track for the bpm and genre
+def bpm_to_song(current_tempo, genre): #global function allows code reusability
+    recommendations = sp.recommendations(
+        seed_genres = genre, #users select desired genre, can have multiple as well
+        target_tempo = current_tempo, #this will be given by pace-to-BPM code
+        min_tempo = current_tempo - 5,
+        max_tempo = current_tempo + 5,
+        limit = 1
+    )
+
+    current_track = recommendations["tracks"][0] #get first track only amongst all recs
+    current_artist = [a['name'] for a in current_track['artists']] #displays only the name of the current artist
+
+    return Song(name = current_track['name'], artists = current_artist, id = current_track['id'])
+
+
+@app.post("/recommendations") #path to recs
+async def track_rec():
+    current_track = bpm_to_song(current_tempo, genre)
+    current_artist = current_artist = [a['name'] for a in current_track['artists']]
+
+    return Song(name = current_track['name'], artists = current_artist, id = current_track['id'])
+
+
+@app.post("/history")
+async def add_history():
+    current_track = bpm_to_song(current_tempo, genre)
+    current_artist = [a['name'] for a in current_track['artists']]
+
+    #add to history
+    track_history.append({
+        'name': current_track['name'],
+        'artists': current_artist,
+        'id': current_track['id']
+    })
+
+    return {"message": "Successfully added track to the history", "track": current_track['name']}
+
+
+@app.get("/history")
+async def get_history():
+    return track_history
