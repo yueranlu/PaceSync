aa284d9a0718f67d74db2d977f8152e25321de25
yueranlu <yueranlu05@gmail.com>
2025-06-23 23:06:23 -0400

    Add files via upload

---
+++ Views/Home Page/Playlist Bar.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+//  PlayList Bar.swift
+//  PaceSync (Frontend)
+//
+//  Created by Yueran Lu on 2025-06-23.
+//
+
+struct PlaylistBar: View{
+    // variable declarations
+    
+    // need variable for each playlist.
+    
+    var Body: some View{
+        ScrollView(.horizontal){
+            LazyHStack{
+                
+            }
+        }
+        
+        
+        // actual view
+    }
+}
