0dd67e8fa90ebe6c96989ee8ce9996c159c9e1d6
endemm <endemm09@gmail.com>
2025-08-12 17:01:54 -0400

    implemented room connect with livekit, and token generation code in python
    tested to work

--- backend/backend-python/Livekit_Server.py
+++ backend/backend-python/Livekit_Server.py
@@ -1,3 +1,20 @@
 import os
 from livekit import api
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/getToken')
+def getToken():
+    token = api.AccessToken(os.getenv('devkey'), os.getenv('secret')) \
+        .with_identity("identity") \
+        .with_name("my name") \
+        .with_grants(api.VideoGrants(
+            room_join=True,
+            room="my-room",
+        ))
+    
+    return token.to_jwt()
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
