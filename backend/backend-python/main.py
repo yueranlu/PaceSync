890e9fae11b26cecfbbf59ca7395a76a1fcd5187
endemm <endemm09@gmail.com>
2025-08-21 17:34:40 -0400

    setting up for next work session

--- backend/backend-python/main.py
+++ backend/backend-python/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+#API routers
+from schemas.sessions_schema import ErrorResponse
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"] #allows requests from any origin (device)
+    allow_origins=["*"], #allows requests from any origin (device)
     allow_credentials=True, #allowing authentication information in requests
     allow_methods=["*"], #allows all HTTP request types (GET, POST, etc.)
-    allow_headers=["*"], 
+    allow_headers=["*"],
 )
 
 app.get("/")
